bd531b1164a6302a8aa858e053279564008fea75
yohanhab <133961585+yohanhab@users.noreply.github.com>
2023-05-24 17:04:52 +0300

    Update dominoPiling.py
    removed the function and changed to simply accept user input

--- dominoPiling.py
+++ dominoPiling.py
@@ -1,11 +1,14 @@
-def dominoPiling(m,n):
-        answer = 0
+input = input()
+nums = []
+for i in input.split():
+    nums.append(int(i))
+if nums[0] %2 == 0:
+        print(int((nums[1])*(nums[0] /2)))
+else:
+    if nums[1] %2 == 0:
+        print(int((nums[1])*((nums[0] -1) /2) +(nums[1]/2)))
+    else:
+        print(int((nums[1])*((nums[0] -1) /2) + ((nums[1] -1) /2)))
 
-        if m %2 == 0:
-            answer += (n)*(m/2)
-        else:
-            if n %2 == 0:
-                answer += (n)*((m -1) /2) + (n/2)
-            else:
-                answer += (n)*((m -1) /2) + ((n -1)/2)
-        return int(answer)
+    
+       
